cfb38a342b203ba401a2e8e7d7bd64ea8bb69627
Adam Kuzikov <49373540+tst-rpstry@users.noreply.github.com>
2022-07-13 10:57:26 +0600

    add: handlers for: \name \bad \data \headers

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -20,6 +20,10 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/name/{PARAM}", nameHandler).Methods(http.MethodGet)
+	router.HandleFunc("/bad", internalServerErrorHandler).Methods(http.MethodGet)
+	router.HandleFunc("/data", dataHandler).Methods(http.MethodPost)
+	router.HandleFunc("/headers", headersHandler).Methods(http.MethodPost)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
@@ -35,4 +39,49 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name, present := params["PARAM"]
+	w.Header().Set("Content-Type", "text/plain")
+
+	if !present {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`$PARAM not found`))
+		return
+	}
+	// default code 200 OK will be set.
+	w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
+}
+
+func internalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(nil)
+}
+
+func dataHandler(w http.ResponseWriter, r *http.Request) {
+	buf := []byte{}
+	_, err := r.Body.Read(buf)
+	if err != nil {
+		http.Error(w, "error reading param", http.StatusBadRequest)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Hello, %s", buf)))
+
+}
+
+func headersHandler(w http.ResponseWriter, r *http.Request) {
+	a, err := strconv.Atoi(r.Header.Get("a"))
+	if err != nil {
+		http.Error(w, "Header 'a' not set.", http.StatusBadRequest)
+		return
+	}
+	b, err := strconv.Atoi(r.Header.Get("b"))
+	if err != nil {
+		http.Error(w, "Header 'b' not set.", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("a+b", strconv.Itoa(a+b))
 }
